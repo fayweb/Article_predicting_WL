8d8ce3df26ec1d4825b3aabcf5d8fc71f82840b4
fayweb <fy.webster@gmail.com>
2023-09-04 16:14:51 +0200

    repairing script 2

--- R/Scripts/3.MICE_imputation.R
+++ R/Scripts/3.MICE_imputation.R
@@ -1,121 +1,27 @@
-## ---------------------------------------------------------------------------------------------------
-library(mice)
-library(VIM)
 
+source("R/Scripts/2.Data_normalization.R")
 
+hm <- hm_norm
 
-## ---------------------------------------------------------------------------------------------------
-hm <- read.csv("Data/Data_output/2.1.norm_MICE_data_set.csv")
+rm(hm_norm)
 
+hm_genes <- rbind(gmf, gml)
 
-## ---------------------------------------------------------------------------------------------------
-# Vectors for selecting genes
-#Lab genes
-# The measurements of IL.12 and IRG6 are done with an other assay and will 
-#ignore for now
-Gene_lab   <- c("IFNy", "CXCR3", "IL.6", "IL.13", # "IL.10",
-                "IL1RN","CASP1", "CXCL9", "IDO1", "IRGM1", "MPO", 
-                "MUC2", "MUC5AC", "MYD88", "NCR1", "PRF1", "RETNLB", "SOCS1", 
-                "TICAM1", "TNF") #"IL.12", "IRG6")
-
-Genes_wild   <- c("IFNy", "CXCR3", "IL.6", "IL.13",# "IL.10", 
-                  "IL1RN","CASP1", "CXCL9", "IDO1", "IRGM1", "MPO", 
-                  "MUC2", "MUC5AC", "MYD88", "NCR1", "PRF1", "RETNLB", "SOCS1", 
-                  "TICAM1", "TNF") #, "IL.12", "IRG6")
-
-Facs_lab <- c("CD4", "Treg", "Div_Treg", "Treg17", "Th1", 
-                    "Div_Th1", "Th17", "Div_Th17", "CD8", "Act_CD8", 
-                    "Div_Act_CD8", "IFNy_CD4", "IFNy_CD8","Treg_prop", 
-                    "IL17A_CD4")  
-
-Facs_wild <- c( "Treg", "CD4", "Treg17", "Th1", "Th17", "CD8",
-                     "Act_CD8", "IFNy_CD4", "IL17A_CD4", "IFNy_CD8")
-
-
-## ---------------------------------------------------------------------------------------------------
-field <- hm %>%
-  dplyr::filter(origin == "Field") 
-
-field <- unique(field)
-genes_mouse_field <- field %>%
-  dplyr::select(c(Mouse_ID, "IFNy", "CXCR3", "IL.6", "IL.13",# "IL.10", 
-                  "IL1RN","CASP1", "CXCL9", "IDO1", "IRGM1", "MPO", 
-                  "MUC2", "MUC5AC", "MYD88", "NCR1", "PRF1", "RETNLB", "SOCS1", 
-                  "TICAM1", "TNF"))
-
-genes <- genes_mouse_field  %>%
-  dplyr::select(-Mouse_ID)
-#remove rows with only nas
-genes <- genes[,colSums(is.na(genes))<nrow(genes)]
-#remove colums with only nas 
-genes <- genes[rowSums(is.na(genes)) != ncol(genes), ]
-genes_mouse_field <- genes_mouse_field[row.names(genes), ]
-##select same rows in the first table
-field <- field[row.names(genes), ]
-
-
-###############lab
-#select the genes and lab muce
-lab <- hm %>%
-  dplyr::filter(origin == "Lab", Position == "mLN") #selecting for mln to avoid
-# duplicates
-lab <- unique(lab)
-gene_lab_mouse <- lab %>%
-  dplyr::select(c(Mouse_ID, "IFNy", "CXCR3", "IL.6", "IL.13",# "IL.10", 
-                  "IL1RN","CASP1", "CXCL9", "IDO1", "IRGM1", "MPO", 
-                  "MUC2", "MUC5AC", "MYD88", "NCR1", "PRF1", "RETNLB", "SOCS1", 
-                  "TICAM1", "TNF"))
-
-gene_lab_mouse <- unique(gene_lab_mouse)
-
-genes_lab <- gene_lab_mouse[, -1]
-
-#remove rows with only nas
-genes_lab <- genes_lab[,colSums(is.na(genes_lab))<nrow(genes_lab)]
-
-#remove colums with only nas 
-genes_lab <- genes_lab[rowSums(is.na(genes_lab)) != ncol(genes_lab), ]
-
-genes_lab <- unique(genes_lab)
-
-#select same rows in the first table
-gene_lab_mouse <- gene_lab_mouse[row.names(genes_lab), ]
-
-##select same rows in the first table
-lab <- lab[row.names(genes_lab), ]
-
-hm_genes <- rbind(gene_lab_mouse, genes_mouse_field)
-
-hm_selection_g <- rbind(lab, field)
-
-genes <- hm_genes %>% 
-  left_join(hm_selection_g %>%
-              dplyr::select(c(Mouse_ID, origin)),
-            by = "Mouse_ID")
-
-
-genes <- genes %>%
-  dplyr::select(-Mouse_ID)
-  
-genes$origin <- as.factor(genes$origin)
+# remove HKG
+hm_genes <- hm_genes %>%
+  dplyr::select(-c(PPIB, GAPDH))
 
 #dplyr::select(-Mouse_ID)
 # looking at patterns of nas)
 #pattern_na <-as.data.frame(md.pattern(field_genes))
 sapply(hm_genes, function(x) sum(is.na(x)))
 
+genes <- hm_genes[,-1]
 
-## ---------------------------------------------------------------------------------------------------
-# Discarding the origin 
-#genes <- genes %>% dplyr::select(-origin)
-
-
-#had to remove as they were disturbing the imputation: Worms_presence, 
-#MC.Eimeria.FEC,  Heligmosomoides_polygurus, Zfy2, Y,  MpiC,
-#vis_miss(field)
 # The frequency distribution of the missing cases per variable can be obtained 
 # as:
 init <- mice(genes, maxit = 0)
+
 #we want to impute only the specific variables
 meth <- init$method
 
